0e482643e9ffefd2aacc5e75a3a3640f4e8943bf
Florian Wagenhofer <devine.root@gmail.com>
2023-01-21 10:04:28 +0100

    Story 1.2 - Methode getGroupLeds() implementiert

--- ledController/src/main/java/at/edu/c02/ledcontroller/LedController.java
+++ ledController/src/main/java/at/edu/c02/ledcontroller/LedController.java
@@ -1,7 +1,9 @@
 package at.edu.c02.ledcontroller;
 
 import java.io.IOException;
+import java.util.List;
 
 public interface LedController {
     void demo() throws IOException;
+    List<Boolean> getGroupLeds() throws IOException;
 }
